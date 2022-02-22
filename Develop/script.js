2e1536478ba08cbe6bf5c2871f5255ea88467a39
Jake Luedke <rajake18@gmail.com>
2022-02-21 19:56:44 -0700

    updated readme and added functionality to the javascript

--- Develop/script.js
+++ Develop/script.js
@@ -1,70 +1,45 @@
 
+$(document).ready(function() {
 
-$('#currentDate').text(moment().format('dddd, MMM Do'));
-
-
-
-
-
-// If the time has already passed, the row class is grey, if it it current time, the row is red, if the 
-// time hasn't happened it is green.
-
-
-//function colorRow() {
-    //var currentHour = moment().hour();
-
-    //$(".description").each(function(){
-        //var blockHour = $(this).parent().attr("id");
-
-            //if (blockHour < currentHour) {
-
-                //console.log(blockHour);
-                //console.log(currentHour);
-                //blockHour.classList.add("past");
-
-            //} else if (blockHour === currentHour) {
-
-                //blockHour.classList.add("present");
-         
-            //} else {
-
-                //blockHour.classList.add("future");
-
-            //};
-    //});
-//};
-
-//function colorRow();
-
-
-
-// When the save button is pressed, the input from the text in that row gets stored into local storage.
-// these are for putting items into local storage
-
-var saveBtn = "";
-
-saveBtn.addEventListener("click", );
-    var saveBtn = $(this);
-
-    if (saveBtn === true) {
+    $('.saveBtn').on('click', function() {
         var time = $(this).parent().attr("id");
         var text = $(this).siblings(".description").val();
+
         localStorage.setItem(time, text);
+    });
+
+
+    function colorRow() {
+        let currentHour = moment().hours();
+
+        $(".time-block").each(function(){
+            let hourBlock = parseInt($(this).attr("id"));
+            
+            if (hourBlock < currentHour) {
+                $(this).addClass('past');
+            } else if (hourBlock === currentHour) {
+                $(this).removeClass('past');
+                $(this).addClass('present');
+            } else {
+                $(this).removeClass('past');
+                $(this).removeClass('present');
+                $(this).addClass('future');
+            }
+        })
     };
+    
+    colorRow();
 
+    $("#9 .description").val(localStorage.getItem("9"));
+    $("#10 .description").val(localStorage.getItem("10"));
+    $("#11 .description").val(localStorage.getItem("11"));
+    $("#12 .description").val(localStorage.getItem("12"));
+    $("#13 .description").val(localStorage.getItem("13"));
+    $("#14 .description").val(localStorage.getItem("14"));
+    $("#15 .description").val(localStorage.getItem("15"));
+    $("#16 .description").val(localStorage.getItem("16"));
+    $("#17 .description").val(localStorage.getItem("17"));
 
+    $('#currentDate').text(moment().format('dddd, MMM Do'));
 
-
-// $("#8 .description").val(localStorage.getItem("8"));
-
-$("#9 .description").val(localStorage.getItem("9"));
-$("#10 .description").val(localStorage.getItem("10"));
-$("#11 .description").val(localStorage.getItem("11"));
-$("#12 .description").val(localStorage.getItem("12"));
-$("#13 .description").val(localStorage.getItem("13"));
-$("#14 .description").val(localStorage.getItem("14"));
-$("#15 .description").val(localStorage.getItem("15"));
-$("#16 .description").val(localStorage.getItem("16"));
-
-
-
+})
